bf4fc7df6ae02ede303de2428c270ffd242c836c
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-08 23:37:10 +0200

    have each top-level directory handled by a different module

--- lib/CPANio/Web.pm
+++ lib/CPANio/Web.pm
@@ -2,10 +2,23 @@ package CPANio::Web;
 
 use Web::Simple;
 
+# the top-level dispatcher
 sub dispatch_request {
 
+    # we're a static site, so we only do GET
     sub (GET) {
-        [ 200, [ 'Content-type', 'text/plain' ], [ "Hello world!\n" ] ]
+
+        # each top-level directory handled by a different module
+        sub (/*/...) {
+            my ( $self, $top, $env ) = @_;
+            eval { require "CPANio/Web/\u$top.pm" }
+                or return [ 404, [ 'Content-type', 'text/plain' ], [ "Not Found" ] ];
+
+            sub (/*) { shift; "CPANio::Web::\u$top"->dispatch(@_) }
+        },
+
+        # not found
+        sub () { [ 404, [ 'Content-type', 'text/plain' ], [ "Not Found" ] ] }
     },
     sub () {
         [ 405, [ 'Content-type', 'text/plain' ], [ "Method not allowed\n" ] ]
