b313de13cb9bd6301854903c56e6372a55b96b47
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-09 19:33:42 +0200

    first proper implementation of the blog
    the path is given the .md extension and looked for under blog_dir

--- lib/CPANio/App/Blog.pm
+++ lib/CPANio/App/Blog.pm
@@ -1,8 +1,12 @@
 package CPANio::App::Blog;
 
 use Web::Simple;
+use Plack::Response;
+use Path::Class;
+use Text::Markdown 'markdown';
 
 sub dispatch_request {
+    my ($self) = @_;
 
     # various index pages
     sub (/)  { ... },
@@ -12,7 +16,13 @@ sub dispatch_request {
     # a blog post to render
     sub (/**) {
         my ( $self, $post, $env ) = @_;
-        [ 200, [ 'Content-type', 'text/plain' ], [$post] ];
+        my $file = file( $self->config->{blog_dir}, $post . '.md' );
+        return Plack::Response->new(404)->finalize if !-e $file;
+
+        [   200,
+            [ 'Content-type', 'text/plain' ],
+            [ markdown( scalar $file->slurp ) ]
+        ];
     }
 
 }
