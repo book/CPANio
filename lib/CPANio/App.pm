9eea81add44dd458d79ae340eae2e0d0e438a8f9
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-10 03:27:51 +0200

    a simple layout wrapper for all .html pages

--- lib/CPANio/App.pm
+++ lib/CPANio/App.pm
@@ -2,15 +2,41 @@ package CPANio::App;
 
 use Web::Simple;
 use Plack::Response;
+use Template;
 
 # cache the various handlers
 my %handler;
+
+# default configuration
+sub default_config {
+    ( ui => \"<html><body>\n[% content %]\n</body></html>", );
+}
 
 # the top-level dispatcher
 sub dispatch_request {
 
     # we're a static site, so we only do GET
     sub (GET) {
+        my ($self) = @_;
+
+        # any .html will be wrapped in the default layout
+        sub (.html) {
+            response_filter {
+                my ($res) = @_;
+
+                # do not deal with streams
+                return if ref $res->[2] ne 'ARRAY';
+
+                my $tt = Template->new;
+                $tt->process(
+                    $self->config->{ui},
+                    { content => join( '', @{ $res->[2] } ) },
+                    \( my $output = "" )
+                ) or die $tt->error();
+                $res->[2] = [$output];
+                return $res;
+            }
+        },
 
         # each top-level directory is handled by a different module
         sub (/*/...) {
