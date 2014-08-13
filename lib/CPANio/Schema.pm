9097771adc90d7260664a6e531b033d3465c54b7
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-13 12:24:54 +0200

    DBIx::Class schema for the once-a-period boards

---
+++ lib/CPANio/Schema.pm
@@ -0,0 +1,7 @@
+package CPANio::Schema;
+use base qw/DBIx::Class::Schema/;
+
+# load all Result classes in Library/Schema/Result/
+__PACKAGE__->load_namespaces();
+
+1;
---
+++ lib/CPANio/Schema/Result/OnceAWeek.pm
@@ -0,0 +1,16 @@
+package CPANio::Schema::Result::OnceAWeek;
+
+use base qw/DBIx::Class::Core/;
+
+__PACKAGE__->table('once_a_week');
+
+__PACKAGE__->add_columns(
+    contest => { data_type => 'text',    is_nullable => 0 },
+    rank    => { data_type => 'integer', is_nullable => 0 },
+    author  => { data_type => 'text',    is_nullable => 0 },
+    count   => { data_type => 'integer', is_nullable => 0 },
+    active  => { data_type => 'boolean', is_nullable => 0 },
+    safe    => { data_type => 'boolean', is_nullable => 0 },
+);
+
+1;
