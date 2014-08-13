8febe8256f85e9ac2d8666e6e13134c0326037ba
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-13 12:24:19 +0200

    sqlite schema for the once-a-period boards

---
+++ cpanio.sql
@@ -0,0 +1,3 @@
+CREATE TABLE once_a_day   ( contest text, rank int, author text, count int, active boolean, safe boolean );
+CREATE TABLE once_a_month ( contest text, rank int, author text, count int, active boolean, safe boolean );
+CREATE TABLE once_a_week  ( contest text, rank int, author text, count int, active boolean, safe boolean );
