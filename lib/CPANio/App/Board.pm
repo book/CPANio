bd4e83fd0d211ca4637b01e59e895170ab1adde7
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-16 12:46:28 +0200

    update of the board application

--- lib/CPANio/App/Board.pm
+++ lib/CPANio/App/Board.pm
@@ -7,26 +7,35 @@ use CPANio::Schema;
 sub dispatch_request {
     my ($self) = @_;
 
-    # various index pages
-    sub (/) {...},
-
     sub (/once-a/*/) {
         my ( $self, $category, $env ) = @_;
 
-        return Plack::Response->new(404)->finalize
-            if $category !~ /^(day|week|month)/;
+        return if $category !~ /^(day|week|month)/;
 
         my $schema = $self->config->{schema};
         my $tt     = $self->config->{template};
-        my $vars
-            = { entries => scalar $schema->resultset("OnceA\u$category")
-                ->search( { contest => 'current' }, { order_by => 'rank' } ),
-            };
-        $tt->process( 'board/once-a.tt', $vars, \my $output )
+        my $vars   = {
+            boards => {
+                map {
+                    (   $_ => {
+                            entries =>
+                                scalar $schema->resultset("OnceA\u$category")
+                                ->search(
+                                { contest  => $_ },
+                                { order_by => 'rank' }
+                                ),
+                            title => $_
+                        }
+                        )
+                    } qw( current all-times )
+            },
+            limit => 250,
+        };
+        $tt->process( 'board/once_a/index', $vars, \my $output )
             or die $tt->error();
 
         [ 200, [ 'Content-type', 'text/html' ], [$output] ];
-    }
+        },
 
 }
 
