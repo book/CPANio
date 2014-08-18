4ca45fc3f66d1f4af5311affdd86ee119e8b5e18
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-19 00:56:52 +0200

    a table for recording the bins for the once-a boards

--- cpanio.sql
+++ cpanio.sql
@@ -1,3 +1,5 @@
+CREATE TABLE once_a_bins  ( bin text NOT NULL, author text NOT NULL default '', count int NOT NULL DEFAULT 0 );
+CREATE UNIQUE INDEX once_a_bins_bin_author ON once_a_bins( bin, author );
 CREATE TABLE once_a_day   ( contest text NOT NULL, rank int NOT NULL, author text NOT NULL, count int NOT NULL, active boolean NOT NULL, safe boolean NOT NULL );
 CREATE TABLE once_a_month ( contest text NOT NULL, rank int NOT NULL, author text NOT NULL, count int NOT NULL, active boolean NOT NULL, safe boolean NOT NULL );
 CREATE TABLE once_a_week  ( contest text NOT NULL, rank int NOT NULL, author text NOT NULL, count int NOT NULL, active boolean NOT NULL, safe boolean NOT NULL );
