5768dde3734b9fe65c1f9a6f674757ecb470055b
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-19 00:56:21 +0200

    make all columns definitions NOT NULL

--- cpanio.sql
+++ cpanio.sql
@@ -1,3 +1,3 @@
-CREATE TABLE once_a_day   ( contest text, rank int, author text, count int, active boolean, safe boolean );
-CREATE TABLE once_a_month ( contest text, rank int, author text, count int, active boolean, safe boolean );
-CREATE TABLE once_a_week  ( contest text, rank int, author text, count int, active boolean, safe boolean );
+CREATE TABLE once_a_day   ( contest text NOT NULL, rank int NOT NULL, author text NOT NULL, count int NOT NULL, active boolean NOT NULL, safe boolean NOT NULL );
+CREATE TABLE once_a_month ( contest text NOT NULL, rank int NOT NULL, author text NOT NULL, count int NOT NULL, active boolean NOT NULL, safe boolean NOT NULL );
+CREATE TABLE once_a_week  ( contest text NOT NULL, rank int NOT NULL, author text NOT NULL, count int NOT NULL, active boolean NOT NULL, safe boolean NOT NULL );
