6d6b84cf04ce171de89850e83162f0fd68cb4e62
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-21 11:57:16 +0200

    add a timestamps table to record the latest runs

--- cpanio.sql
+++ cpanio.sql
@@ -2,3 +2,4 @@ CREATE TABLE once_a_bins  ( bin text NOT NULL, author text NOT NULL default '', 
 CREATE TABLE once_a_day   ( contest text NOT NULL, rank int NOT NULL, author text NOT NULL, count int NOT NULL, active boolean NOT NULL, safe boolean NOT NULL );
 CREATE TABLE once_a_month ( contest text NOT NULL, rank int NOT NULL, author text NOT NULL, count int NOT NULL, active boolean NOT NULL, safe boolean NOT NULL );
 CREATE TABLE once_a_week  ( contest text NOT NULL, rank int NOT NULL, author text NOT NULL, count int NOT NULL, active boolean NOT NULL, safe boolean NOT NULL );
+CREATE TABLE timestamps   ( board text UNIQUE NOT NULL, latest_update datetime NOT NULL, PRIMARY KEY ( board ) );
