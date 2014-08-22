3e43da3803a052c29bea4f4d35548f0746dde1c3
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-23 01:01:40 +0200

    _compute_boards_current replaces _find_current_chains
    this is using the chains found by _find_authors_chains

--- lib/CPANio/Board/OnceA.pm
+++ lib/CPANio/Board/OnceA.pm
@@ -31,67 +31,6 @@ sub _get_bins_for {
     ];
 }
 
-sub _find_current_chains {
-
-    for my $category (@CATEGORIES) {
-
-        # pick the bins for the current category
-        my @bins = @{ _get_bins_for($category) };
-
-        # only pick the users having at least a relase in the latest bins
-        my @authors = $CPANio::schema->resultset('ReleaseBins')->search(
-            { author => { '!=' => '' }, bin => [ @bins[ 0, 1 ] ] },
-            { group_by => 'author', order_by => { -asc => 'author' } }
-        )->get_column('author')->all;
-
-        # get the list of bins for all the selected authors
-        my %bins;
-        push @{ $bins{ $_->author } }, $_->bin
-            for $CPANio::schema->resultset('ReleaseBins')->search(
-            {   author => { -in  => \@authors },
-                bin    => { like => $LIKE{$category} }
-            },
-            { order_by => { -desc => 'bin' } }
-            );
-
-        # compute each author's current chain
-        my @entries;
-        for my $author ( keys %bins ) {
-            push @entries, my $entry = {
-                author  => $author,
-                contest => 'current',
-                active  => 0,
-            };
-
-            ( $entry->{safe}, my $i, my $j ) = $bins{$author}[0] eq $bins[0]
-                ? ( 1, 0, 0 )    # safe, count from the latest bin
-                : ( 0, 1, 0 );   # not safe yet, count from penultimate bin
-
-            # move till the end of the current chain
-            ( $i++, $j++ ) while $bins[$i] eq ( $bins{$author}[$j] // '' );
-            $entry->{count} = $j;
-        }
-
-        # sort chains and weed out the short ones
-        @entries = sort { $b->{count} <=> $a->{count} }
-            grep { $_->{count} >= 2 } @entries;
-
-        # compute rank
-        my $Rank = my $rank = my $prev = 0;
-        for my $entry (@entries) {
-            $Rank++;
-            $rank          = $Rank if $entry->{count} != $prev;
-            $prev          = $entry->{count};
-            $entry->{rank} = $rank;
-        }
-
-        # update database
-        my $rs = $CPANio::schema->resultset("OnceA\u$category");
-        $rs->search( { contest => 'current' } )->delete();
-        $rs->populate( \@entries );
-    }
-}
-
 sub _find_authors_chains {
     my $schema  = $CPANio::schema;
     my $bins_rs = $schema->resultset('ReleaseBins');
@@ -136,6 +75,48 @@ sub _find_authors_chains {
     return \%chains;
 }
 
+sub _compute_boards_current {
+    my ($chains) = @_;
+
+    for my $category (@CATEGORIES) {
+
+        # pick the bins for the current category
+        my $bins = _get_bins_for($category);
+
+        # only keep the active chains
+        my @entries;
+        for my $author ( keys %{ $chains->{$category} } ) {
+            my $chain = $chains->{$category}{$author}[0]; # current chain only
+            if ( $chain->[0] eq $bins->[0] || $chain->[0] eq $bins->[1] ) {
+                push @entries, {
+                    contest => 'current',
+                    author  => $author,
+                    count   => scalar @$chain,
+                    safe    => 0 + ( $chain->[0] eq $bins->[0] ),
+                    active  => 0,
+                    };
+            }
+        }
+
+        # sort chains
+        @entries = sort { $b->{count} <=> $a->{count} } @entries;
+
+        # compute rank
+        my $Rank = my $rank = my $prev = 0;
+        for my $entry (@entries) {
+            $Rank++;
+            $rank          = $Rank if $entry->{count} != $prev;
+            $prev          = $entry->{count};
+            $entry->{rank} = $rank;
+        }
+
+        # update database
+        my $rs = $CPANio::schema->resultset("OnceA\u$category");
+        $rs->search( { contest => 'current' } )->delete();
+        $rs->populate( \@entries );
+    }
+}
+
 # CLASS METHODS
 sub board_name { 'once-a' }
 
@@ -146,7 +127,10 @@ sub update {
     require CPANio::Board::Bins;
     return if $since > CPANio::Board::Bins->latest_update;
 
-    _find_current_chains();
+    # pick up all the chains
+    my $chains = _find_authors_chains();
+    _compute_boards_current($chains);
+
     __PACKAGE__->update_done();
 }
 
