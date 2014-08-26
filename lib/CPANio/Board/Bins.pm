4bf3a8e412b4ff0b7cd37760923658a5304b6c81
Philippe Bruhat (BooK) <book@cpan.org>
2014-08-26 21:15:07 +0200

    possibly a speed optimisation, but it sure cuts off a few lines

--- lib/CPANio/Board/Bins.pm
+++ lib/CPANio/Board/Bins.pm
@@ -35,16 +35,12 @@ sub _final_week_number {
 
 # given an epoch time, return all the bins the dist belongs to
 sub _datetime_to_bins {
-    my $dt    = shift;
-    my $year  = $dt->year;
-    my $month = sprintf '%02d', $dt->month;
-    my $day   = sprintf '%02d', $dt->day;
-    my $hour  = sprintf '%02d', $dt->hour;
+    my $dt = shift;
+    my ( $year, $month, $day, $week_year, $week_number ) = split / /,
+        $dt->strftime('%Y %m %d %Y %U');
 
-    # these are needed for the weekly stuff
-    my $week_number = $dt->strftime('%U');
-    my $week_year   = $year;
-    if ($week_number == 0) {
+    # week 0 is actually the last week of the previous year
+    if ( $week_number == 0 ) {
         $week_year--;
         $week_number = _final_week_number($week_year);
     }
